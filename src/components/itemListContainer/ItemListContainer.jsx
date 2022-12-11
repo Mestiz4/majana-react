97aa06c7776bbef66f75902a747d9164f75e3c4f
Mestiz4 <info.mestiz4@gmail.com>
2022-12-10 22:56:09 -0500

    navbar y productos en back

--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import CustomButton from '../customButton/CustomButton'
+import React, { useState } from 'react'
+
+import { products } from '../../products'
+
+
 
 const ItemListContainer = ( { greeting } ) => {
+
+    const [items, setItems] = useState(0)
+
+    const task = new Promise((resolve, reject) => {
+        resolve(products)
+    })
+
+    task
+        .then ((res)=>{console.log(res)})
+        .catch ((err)=>{console.log("se rechazo")})
+
+    console.log(products)
+
   return (
     <div>
     <h2>{greeting}</h2>
-    <CustomButton />
+    
     </div>
   )
 }
